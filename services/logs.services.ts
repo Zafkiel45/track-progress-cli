2578bc3d512928abf8ebc023a38d9510195c9bb0
zafkiel45 <zafkielc@gmail.com>
2025-05-08 17:41:03 -0300

    chore(history): prettier applied

--- services/logs.services.ts
+++ services/logs.services.ts
@@ -3,46 +3,50 @@ import type { History } from "../types/history.types";
 import { formatTextForDatabase } from "../utils/goals.utils";
 import { iterateOverLogs } from "../utils/interateOverLogs";
 
-export function registerLogService(type: string, target: string, datetime: string) {
+export function registerLogService(
+  type: string,
+  target: string,
+  datetime: string
+) {
   try {
     const query = database.prepare(`
       INSERT INTO history (type, target, datetime) 
       VALUES (@type, @target, @datetime)
-    `); 
-  
+    `);
+
     database.transaction(() => {
       query.run({
-        type: type, 
+        type: type,
         target: target,
-        datetime: datetime, 
+        datetime: datetime,
       });
     })();
-  
-    console.log('✅ activity registred on history');
-  } catch(err) {
+
+    console.log("✅ activity registred on history");
+  } catch (err) {
     console.error(err);
-  };
-};
-
-
+  }
+}
 
 export function showHistoryService(type: string, from?: string, to?: string) {
-  if(typeof from === 'undefined' || typeof to === 'undefined') {
-    if(formatTextForDatabase(type) === 'all') {
-      const query = database.query('SELECT type,target,datetime FROM history');
-      const logs  = query.all() as History[];
+  if (typeof from === "undefined" || typeof to === "undefined") {
+    if (formatTextForDatabase(type) === "all") {
+      const query = database.query("SELECT type,target,datetime FROM history");
+      const logs = query.all() as History[];
 
       iterateOverLogs(logs);
     } else {
       const query = database.query(`
         SELECT type,target,datetime FROM history WHERE type = @type  
       `);
-      const logs  = query.all({type: formatTextForDatabase(type)}) as History[];
+      const logs = query.all({
+        type: formatTextForDatabase(type),
+      }) as History[];
 
       iterateOverLogs(logs);
     }
-  } else if(typeof to === 'string' && typeof from === 'string') {
-    if(formatTextForDatabase(type) === 'all') {
+  } else if (typeof to === "string" && typeof from === "string") {
+    if (formatTextForDatabase(type) === "all") {
       const query = database.query(`
         SELECT type,target,datetime 
         FROM history 
@@ -50,9 +54,9 @@ export function showHistoryService(type: string, from?: string, to?: string) {
         BETWEEN @from AND @to
       `);
 
-      const logs  = query.all({
-        to: formatTextForDatabase(to), 
-        from: formatTextForDatabase(from)
+      const logs = query.all({
+        to: formatTextForDatabase(to),
+        from: formatTextForDatabase(from),
       }) as History[];
 
       iterateOverLogs(logs);
@@ -64,8 +68,8 @@ export function showHistoryService(type: string, from?: string, to?: string) {
         BETWEEN @from AND @to AND type = @type
       `);
 
-      const logs  = query.all({
-        to: formatTextForDatabase(to), 
+      const logs = query.all({
+        to: formatTextForDatabase(to),
         from: formatTextForDatabase(from),
         type: formatTextForDatabase(type),
       }) as History[];
@@ -73,7 +77,8 @@ export function showHistoryService(type: string, from?: string, to?: string) {
       iterateOverLogs(logs);
     }
   } else {
-    console.log('❌ You forgot to pass two dates. Example "2025-07-08 17:20:00"');
+    console.log(
+      '❌ You forgot to pass two dates. Example "2025-07-08 17:20:00"'
+    );
   }
-};
-
+}
